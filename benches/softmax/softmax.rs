a36233a0a96aafa199439160e27f46c487c098cc
Stephen Merity <smerity@smerity.com>
2024-05-08 07:33:30 -0700

    Rust, your SIMD disappoints me :'( lol

--- benches/softmax/softmax.rs
+++ benches/softmax/softmax.rs
@@ -5,15 +5,15 @@ use std::hint::black_box;
 use std::io::Read;
 use std::mem::MaybeUninit;
 use std::simd::num::SimdFloat;
-use std::simd::{f64x2, StdFloat};
+use std::simd::StdFloat;
 
 #[allow(non_upper_case_globals)]
-const bench_size: usize = 1000000;
+const bench_size: usize = 8192;
 
-const SIMD_COUNT: usize = bench_size / 64;
-
-// Set the SIMD type
-type SimdType = f64x2;
+use std::simd::f64x64;
+type SimdType = f64x64;
+const SIMD_SIZE: usize = 64;
+const SIMD_COUNT: usize = bench_size / SIMD_SIZE;
 
 fn softmax(x: &[f64; bench_size]) -> [f64; bench_size] {
     let mut max = x[0];
@@ -39,6 +39,20 @@ fn softmax(x: &[f64; bench_size]) -> [f64; bench_size] {
 }
 
 fn softmax_simd(x: &[SimdType; SIMD_COUNT]) -> [SimdType; SIMD_COUNT] {
+    let max: f64 = *x
+        .map(|v| v.reduce_max())
+        .iter()
+        .max_by(|a, b| a.partial_cmp(b).unwrap())
+        .unwrap();
+    let max = SimdType::splat(max);
+
+    let x_exp = x.map(|v| (v - max).exp());
+    let x_exp_sum = x_exp.iter().map(|v| v.reduce_sum()).sum();
+    let divide_by = SimdType::splat(x_exp_sum);
+    x_exp.map(|v| v / divide_by)
+}
+
+fn softmax_simd_fold(x: &[SimdType; SIMD_COUNT]) -> [SimdType; SIMD_COUNT] {
     let max = x
         .iter()
         .fold(SimdType::splat(f64::NEG_INFINITY), |acc, v| {
@@ -48,13 +62,34 @@ fn softmax_simd(x: &[SimdType; SIMD_COUNT]) -> [SimdType; SIMD_COUNT] {
     let max = SimdType::splat(max);
 
     let x_exp = x.map(|v| (v - max).exp());
-    let x_exp_sum = x_exp
-        .iter()
-        .fold(SimdType::splat(0.0), |acc, v| acc + v)
-        .reduce_sum();
+    let x_exp_sum = x_exp.iter().sum::<SimdType>().reduce_sum();
     let divide_by = SimdType::splat(x_exp_sum);
 
     x_exp.map(|v| v / divide_by)
+}
+
+fn softmax_simd_fold_res(x: &[SimdType; SIMD_COUNT], res: &mut [SimdType; SIMD_COUNT]) -> f64 {
+    let max = x
+        .iter()
+        .fold(SimdType::splat(f64::NEG_INFINITY), |acc, v| {
+            acc.simd_max(*v)
+        })
+        .reduce_max();
+    let max = SimdType::splat(max);
+
+    let mut x_exp_sum: SimdType = SimdType::splat(0.0);
+    for (slot, v) in res.iter_mut().zip(x.iter()) {
+        *slot = (*v - max).exp();
+        x_exp_sum += *slot;
+    }
+    let x_exp_sum = x_exp_sum.reduce_sum();
+
+    let divide_by = SimdType::splat(x_exp_sum);
+
+    res.iter_mut().for_each(|v| *v = *v / divide_by);
+    // If we don't do this ... it doesn't materialize the array ..?
+    // This would be a tad faster maybe - but not by as much as Mojooooo -_-
+    res.iter().sum::<SimdType>().reduce_sum()
 }
 
 fn main() {
@@ -103,8 +138,9 @@ fn main() {
     };
 
     for i in 0..SIMD_COUNT {
-        simd_arr[i] = SimdType::from_slice(&arr[i * 64..]);
+        simd_arr[i] = SimdType::from_slice(&arr[i * SIMD_SIZE..]);
     }
+    println!("Length of SIMD array: {}", simd_arr.len());
 
     // benchmark this 1000 times, get mean
     let start = std::time::Instant::now();
@@ -115,6 +151,35 @@ fn main() {
 
     println!(
         "Mean time   (SIMD): {}ms",
+        elapsed as f64 / 1000.0 / 1000.0 / count as f64
+    );
+
+    // benchmark this 1000 times, get mean
+    let start = std::time::Instant::now();
+    for _ in 0..count {
+        black_box(softmax_simd_fold(&simd_arr));
+    }
+    let elapsed = start.elapsed().as_nanos();
+
+    println!(
+        "Mean time   (SIMD fold): {}ms",
+        elapsed as f64 / 1000.0 / 1000.0 / count as f64
+    );
+
+    let mut res_arr: [SimdType; SIMD_COUNT] = unsafe {
+        #[allow(invalid_value)]
+        MaybeUninit::uninit().assume_init()
+    };
+
+    // benchmark this 1000 times, get mean
+    let start = std::time::Instant::now();
+    for _ in 0..count {
+        black_box(softmax_simd_fold_res(&simd_arr, &mut res_arr));
+    }
+    let elapsed = start.elapsed().as_nanos();
+
+    println!(
+        "Mean time   (SIMD fold res): {}ms",
         elapsed as f64 / 1000.0 / 1000.0 / count as f64
     );
 }
